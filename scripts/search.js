d143d32ebf29964234a1baacc9f86e6c30de4a14
amaterasu-development <virgilehayez@gmail.com>
2026-03-27 16:47:31 +0100

    Add new files and finishing javascript code
    That's all

--- scripts/search.js
+++ scripts/search.js
@@ -1,39 +1,44 @@
-let elementsForSearch = ["Apple", "Microsoft", "Facebook", "Google", "Amazon"]
-
 let searchbars = document.querySelectorAll("[data-filter='search']")
-
 let output = document.querySelectorAll("ul.results")
 
-function setupSearchBar(searchelements) {
+export function setupSearchBar(searchElements, errorMessage) {
+    searchbars.forEach((searchInput) => {
+        searchInput.addEventListener("keyup", () => {
+            let searchValue = searchInput.value.toLowerCase();
 
-}
-
-searchbars.forEach((searchInput) => {
-    searchInput.addEventListener("keyup", () => {
-        
-        let searchValue = searchInput.value.toLowerCase()
-        
-        //Clear content
-        output.forEach(searchbar => {
-            searchbar.innerHTML = ""
-        })
-        
-        if (searchValue.trim() == "") return;
-
-        let filter = elementsForSearch.filter(element => element.toLowerCase().includes(searchValue));
-
-        if (filter == "") {
+            // Clear content
             output.forEach(searchbar => {
-                searchbar.innerHTML = "<p>Aucun contenu trouvé"
-            })
-        }
+                searchbar.innerHTML = "";
+            });
 
-        filter.forEach(resultOfSearch => {
-            output.forEach(placeOutput => {
-                let results = document.createElement("li")
-                results.textContent = resultOfSearch
-                placeOutput.appendChild(results)
-            })
-        })
-    })
-})
+            if (searchValue.trim() === "") return;
+
+            // Filtre les éléments dont textElement contient searchValue
+            let filter = searchElements.filter(element =>
+                element.textElement.toLowerCase().includes(searchValue)
+            );
+
+            // Vérifie si le tableau filtré est vide
+            if (filter.length === 0) {
+                output.forEach(searchbar => {
+                    searchbar.innerHTML = "<p class='errorMessage'>" + errorMessage + "</p>";
+                });
+            }
+
+            // Affiche les résultats
+            filter.forEach(resultOfSearch => {
+                output.forEach(placeOutput => {
+                    let results = document.createElement("li");
+                    let link = document.createElement("a")
+                    if(resultOfSearch.isLinkExternal == true) {
+                        link.target = "_blank"
+                    }
+                    link.textContent = resultOfSearch.textElement; // Affiche la propriété textElement
+                    link.href = resultOfSearch.link
+                    results.appendChild(link)
+                    placeOutput.appendChild(results);
+                });
+            });
+        });
+    });
+}
